5e42f560875b9283e37eef9a60ffb06f4e70ae0c
Sitangshu Bhattacharya <32301518+SitangshuIIITA@users.noreply.github.com>
2022-09-07 17:44:25 +0530

    Add files via upload

--- work_dir/run.sh
+++ work_dir/run.sh
@@ -31,27 +31,27 @@ cd ../YAM_RUN
 
 #		sh hf.sh						#Hartree-Fock energy computation.
 
-#		sh gw.sh						#GW calculation (includes self-consistent GW)
+#		sh gw.sh						#GW calculation (includes 4 self-consistent GW. Conduction and valence slopes using regression method is also computed.)
 
 #		sh bse_gw.sh						#BSE+GW calculation
 
 #		sh wf_exciton.sh					#exciton wavefunction/amplitude, etc.
 
-#		sh bse_scissor.sh                                       #BSE+GW calculation
+#		sh bse_scissor.sh                                       #BSE calculation using the automatic GW slope regression evaluation.
 
-#		sh exciton_disp.sh					#exciton dispersion. 
+#		sh exciton_disp.sh					#exciton dispersion calculation. 
 
-#		sh elph.sh						#elph calculation.
+#		sh elph.sh						#elph phonon+Fan+Debye Waller for zero point energy calculation. Phonon double grid is also computed for emission processes. See yambo tutorials.
 
-#		sh eliash.sh
+#		sh eliash.sh						#Electronic Eliashberg calculations for both direct and indirect conduction and valence bands.
 
-#		sh bse_temp_inv.sh					#BSE Temperature calculation using double grid and inversion solver
+#		sh bse_temp_inv.sh					#BSE Temperature calculation (in loop) using double grid and inversion solver with merged GW+elph database.
 
-#		sh bse_temp_diago.sh					#BSE temperature calculation using daigo-solver
+#		sh bse_temp_diago.sh					#BSE temperature calculation (in loop) using daigo-solver with merged GW+elph database.
 
-#		sh exciton_eliash.sh					#Exciton-eliashberg calculation at various temperatures
+#		sh exciton_eliash.sh					#Exciton-eliashberg calculation at various temperatures.
 
-#		sh bse_temp_slepc.sh                                    #BSE Temperature calculation using slepC  solver
+#		sh bse_temp_slepc.sh                                    #BSE Temperature calculation (loop) using slepC solver with merged GW+elph database.
 	
 
 
