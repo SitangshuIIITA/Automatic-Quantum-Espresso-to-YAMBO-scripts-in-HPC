eb4622ff855f06c2523a97c078405c40f075553a
Sitangshu Bhattacharya <32301518+SitangshuIIITA@users.noreply.github.com>
2022-09-08 10:19:15 +0530

    Minor bug is fixed now

--- core_files/YAM_RUN/exciton_disp.sh
+++ core_files/YAM_RUN/exciton_disp.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+#PBS -u sitangshu
+#PBS -N sitangshu
+#PBS -q core160
+#PBS -o out.log
+#PBS -l nodes=4:ppn=40
+#PBS -j oe
+#PBS -V
+     cd $PBS_O_WORKDIR
+        module load compilers/intel/parallel_studio_xe_2018_update3_cluster_edition
+        module load compilers/gcc-7.5.0
+     cat $PBS_NODEFILE |uniq > nodes
 
 
 	mkdir exc_disp 
@@ -5,7 +17,7 @@
       	PATH5=$DIR/band_structure
       	PATH7=$DIR/BSE_GW
      	PATH15=$DIR/exc_disp	
-       
+      
 	cd $DIR/graph_data/
 		mkdir exciton_disp_data
 
@@ -135,47 +147,48 @@
 
 						
 					$MPIRUN_YAMBO yambo -F $exc_disp_filename -J output -C Report
-						
+       					
 			cd $DIR/graph_data/
 
 						mkdir exciton_disp_data
 
-			cd $PATH15/Report
-						scp -r o-output.alpha* $DIR/graph_data/exciton_disp/
+       		cd $PATH15/Report
+       					scp -r o-output.alpha* $DIR/graph_data/exciton_disp_data/
 
-			#-------------------------Excitonic dispersion-------------------------------#
+       		#-------------------------Excitonic dispersion-------------------------------#
 
+       		cd $PATH15
 
-                                        ypp -e i -F $prefix.ypp_exciton_disp.in
+                                       ypp -e i -F $prefix.ypp_exciton_disp.in
 
-							sed -i '18s/INTERP_mode= "NN"/INTERP_mode= "BOLTZ"/g'			$prefix.ypp_exciton_disp.in
-							sed -i '19s/INTERP_Shell_Fac= 20.00000 /INTERP_Shell_Fac= 50.00000 /g'  $prefix.ypp_exciton_disp.in
-                                                        sed -i '21s/BANDS_steps= 10/BANDS_steps= 100/g'                         $prefix.ypp_exciton_disp.in
-							sed -i 's/States= "0 - 0"/States="'"$exciton_states"'"/g'               $prefix.ypp_exciton_disp.in
-							
-							awk '/Grid dimensions/ {print $4*3}' r_setup > 1
-							kcountvar=$(cat 1)
-			#--The following 2 lines are showing interpolation error in ypp calculation.-------------#
-#	                       				sed -i '26s/-1 |-1 |-1 |/'$kcountvar' |'$kcountvar' |1 |/g'		$prefix.ypp_exciton_disp.in
-#							sed -i '28s/#PrtDOS/PrtDOS/g'                               		$prefix.ypp_exciton_disp.in
-			cd $DIR
-                                        		scp -r $prefix.band_route $PATH15/.
-                        cd $PATH15
+       						sed -i '18s/INTERP_mode= "NN"/INTERP_mode= "BOLTZ"/g'                          $prefix.ypp_exciton_disp.in
+                                                       sed -i '19s/INTERP_Shell_Fac= 20.00000 /INTERP_Shell_Fac= 50.00000 /g'  $prefix.ypp_exciton_disp.in
+                                                       sed -i '21s/BANDS_steps= 10/BANDS_steps= 100/g'                         $prefix.ypp_exciton_disp.in
+                                                       sed -i 's/States= "0 - 0"/States="'"$exciton_states"'"/g'               $prefix.ypp_exciton_disp.in
 
-                                        		sed -i.bak '35s/.*//' 							$prefix.ypp_exciton_disp.in
-                                        		cat $prefix.band_route >> $prefix.ypp_exciton_disp.in 
-                                        		sed -i.bak '35d' 							$prefix.ypp_exciton_disp.in
-                                        		sed -i.bak -e '$a%' 							$prefix.ypp_exciton_disp.in
-							rm -rf 1 *.bak
+                                                       awk '/Grid dimensions/ {print $4*3}' r_setup > 1
+                                                       kcountvar=$(cat 1)
+                       #--The following 2 lines are showing interpolation error in ypp calculation.-------------#
+                                                       sed -i '26s/-1 |-1 |-1 |/'$kcountvar' |'$kcountvar' |1 |/g'             $prefix.ypp_exciton_disp.in
+                                                       sed -i '28s/#PrtDOS/PrtDOS/g'                                           $prefix.ypp_exciton_disp.in
+                        
+       		                                scp -r $DIR/$prefix.band_route /.
 
-                                        ypp -F $prefix.ypp_exciton_disp.in -J output
-
-							mv o-output.excitons_interpolated $prefix.o-output.excitons_interpolated
-
-							scp -r $prefix.o-output.excitons_interpolated $DIR/graph_data/exciton_disp_data/.
+                                                       sed -i.bak '35s/.*//'                                                   $prefix.ypp_exciton_disp.in
+                                                       cat $prefix.band_route >> $prefix.ypp_exciton_disp.in
+                                                       sed -i.bak '35d'                                                        $prefix.ypp_exciton_disp.in
+                                                       sed -i.bak -e '$a%'                                                     $prefix.ypp_exciton_disp.in
+       						rm -rf 1 *.bak
 
 
+                                       ypp -F $prefix.ypp_exciton_disp.in -J output
+
+       						mv o-output.excitons_interpolated $prefix.o-output.excitons_interpolated
+
+       						scp -r $prefix.o-output.excitons_interpolated $DIR/graph_data/exciton_disp_data/.
 
 
 
-exit;
+
+
+xit;
