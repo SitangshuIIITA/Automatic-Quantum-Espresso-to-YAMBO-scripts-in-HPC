bef4cc8ae1b4994efe170063afc4d24756974793
Sitangshu Bhattacharya <32301518+SitangshuIIITA@users.noreply.github.com>
2022-12-22 18:11:26 +0530

    Electron stability script: bug fixed.

--- core_files/QE_RUN/QE_electron_stability.sh
+++ core_files/QE_RUN/QE_electron_stability.sh
@@ -1,24 +1,23 @@
-
-  
+       
+       
        mkdir electronic_stability
 
-       PATH1=$DIR/relax	
+       PATH1=$DIR/relax
        PATH2=$DIR/electronic_stability
 
-		cd $PATH2
+                cd $PATH2
 
-			for line in $atoms
-	                     do
+                        for line in $atoms
+                             do
                                 mkdir "${line}_stability"
-				
-			cd ${line}_stability
-				
+
+                                     cd ${line}_stability
 cat > ${line}.relax << EOF
 &CONTROL
-	 			calculation  = 'scf',
-  				     prefix  = '$prefix',
-  				pseudo_dir   = '$PSEUDO_DIR',
-  			 	   verbosity = 'high',
+                                calculation  = 'scf',
+                                     prefix  = '$prefix',
+                                pseudo_dir   = '$PSEUDO_DIR',
+                                   verbosity = 'high',
                                   wf_collect = .true.
                                forc_conv_thr = 1e-05
                                etot_conv_thr = 1e-05
@@ -26,18 +25,18 @@ cat > ${line}.relax << EOF
 
 /
 &SYSTEM 
-  				   ibrav     = 1,
-  				   celldm(1) = 30.0, 
-  				   nat       = 1, 
-  				   ntyp      = 1,
-  				   ecutwfc   = '$ecutwfc',
-  				 occupations ='smearing', 
-				    smearing ='gauss', 
-				      degauss=0.02,
+                                   ibrav     = 1,
+                                   celldm(1) = 30.0, 
+                                   nat       = 1, 
+                                   ntyp      = 1,
+                                   ecutwfc   = $ecutwfc,
+                                 occupations ='smearing', 
+                                    smearing ='gauss', 
+                                      degauss=0.02,
 /
 
 &ELECTRONS
-  				conv_thr    = 1e-10,
+                                conv_thr    = 1e-10,
 /
 /
 ATOMIC_SPECIES
@@ -48,18 +47,17 @@ ${line}  0.000000000000 0.000000000000 0.000000000000
 
 K_POINTS {automatic}
 1 1 1 0 0 0
-EOF	
+EOF
 
-				$MPIRUN_PW ${line}.relax > ${line}.relax.out
-				grep 'Final enthalpy' ${line}.relax.out > ${line}.enthalpy
-				scp -r ${line}.enthalpy $PATH2
 
-		
-			 done
-			
-			 	scp -r $PATH1/relax.out ./.
-				grep 'Final enthalpy' relax.out > $prefix.enthalpy	
+                                $MPIRUN_PW ${line}.relax > ${line}.relax.out
+                                grep '!    total energy' ${line}.relax.out > ${line}.enthalpy
+                                scp -r ${line}.enthalpy $PATH2
 
+                                cd ..
+                         done
+
+                                scp -r $PATH1/relax.out ./.
+                                grep 'Final enthalpy' relax.out > $prefix.enthalpy
+				rm -rf relax.out
 exit;
-
-
