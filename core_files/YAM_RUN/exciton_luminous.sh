1aa3d43e54b64144d6fc65177e4cd7be1165cc1e
Sitangshu Bhattacharya <32301518+SitangshuIIITA@users.noreply.github.com>
2022-12-27 10:56:25 +0530

    Exciton-phonon assisted photoluminescence added

--- core_files/YAM_RUN/exciton_luminous.sh
+++ core_files/YAM_RUN/exciton_luminous.sh
@@ -1,48 +1,36 @@
  
+#	Note: Before ruuning this, you should run the BSE finite q calculation and double-grid electron phonon coupling, i.e., the elph.sh script.
+#	See the Yambo tutorials for more details.
 
-      	PATH5=$DIR/band_structure
-      	PATH7=$DIR/BSE_GW
-     	PATH15=$DIR/exc_disp	
-      
-	cd $DIR/graph_data/
-		mkdir exciton_luminious
+        cd $DIR/exc_disp
 
+        PATH1=$DIR/ELPH/phonon
+        PATH5=$DIR/band_structure
+        PATH16=$DIR/exc_disp
 
-	# Note: Run the BSE for excitons with q/=0 ible-grid.ndb is inside the SAVE folder. But this is ineffective in BSE SlepC/diagonalization computation. However, do not delete this.
-
-
-      		cd $PATH15
- 
-                                       	ypp -e i -F $prefix.ypp_exciton_disp.in
-
-       						       sed -i '18s/INTERP_mode= "NN"/INTERP_mode= "BOLTZ"/g'			$prefix.ypp_exciton_disp.in
-                                                       sed -i '19s/INTERP_Shell_Fac= 20.00000 /INTERP_Shell_Fac= 50.00000 /g'  	$prefix.ypp_exciton_disp.in
-                                                       sed -i '21s/BANDS_steps= 10/BANDS_steps= 100/g'                         	$prefix.ypp_exciton_disp.in
-                                                       sed -i 's/States= "0 - 0"/States="'"$exciton_states"'"/g'               	$prefix.ypp_exciton_disp.in
-
-                                                       awk '/Grid dimensions/ {print $4*3}' r_setup > 1
-                                                       kcountvar=$(cat 1)
-                       #--The following 2 lines are showing interpolation error in ypp calculation.-------------#
-#                                                       sed -i '26s/-1 |-1 |-1 |/'$kcountvar' |'$kcountvar' |1 |/g'             $prefix.ypp_exciton_disp.in
-#                                                       sed -i '28s/#PrtDOS/PrtDOS/g'                                           $prefix.ypp_exciton_disp.in
-                        
-       		                                scp -r $DIR/$prefix.band_route /.
-
-                                                       sed -i.bak '35s/.*//'                                                   	$prefix.ypp_exciton_disp.in
-                                                       cat $prefix.band_route >> $prefix.ypp_exciton_disp.in
-                                                       sed -i.bak '35d'                                                        	$prefix.ypp_exciton_disp.in
-                                                       sed -i.bak -e '$a%'                                                     	$prefix.ypp_exciton_disp.in
-       						rm -rf 1 *.bak
+        cd $PATH16
 
 
-                                       ypp -F $prefix.ypp_exciton_disp.in -J output
+                for line in $temp_value
+                        do
 
-       						mv o-output.excitons_interpolated $prefix.o-output.excitons_interpolated
+                                        ypp_ph -e p -F                                                                  $prefix.ypp.Lumin-${line}$Kelvin.in
 
-       						scp -r $prefix.o-output.excitons_interpolated $DIR/graph_data/exciton_disp_data/.
+                                        sed -i 's/States= "0 - 0"/States="'"$exciton_states"'"/g'                       $prefix.ypp.Lumin-${line}$Kelvin.in
+                                        sed -i 's/BoseTemp=-1.000000         eV/BoseTemp='${line}' K/g'                 $prefix.ypp.Lumin-${line}$Kelvin.in
+                                        sed -i 's/PHfreqF= "none"/PHfreqF= "'"..\/ELPH\/phonon\/$prefix.freq"'"/g'      $prefix.ypp.Lumin-${line}$Kelvin.in
+                                        sed -i 's/DOS_broad= 0.100000/DOS_broad= '$dos_broad_exc_phon'/g'               $prefix.ypp.Lumin-${line}$Kelvin.in
+                                        sed -i 's/DOSESteps=  500/DOSESteps=  1000/g'                                   $prefix.ypp.Lumin-${line}$Kelvin.in
+
+                                        sed -i.bak -e '23d'                                                             $prefix.ypp.Lumin-${line}$Kelvin.in
+                                        sed -i.bak "23i $dos_energy_range"                                              $prefix.ypp.Lumin-${line}$Kelvin.in
 
 
+                                        ypp_ph -F $prefix.ypp.Lumin-${line}$Kelvin.in -J output
 
 
+                        done
 
+
+                                        rm -rf *.bak
 exit;
